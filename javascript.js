6a27a7603e505235fadb605f4109d11c1576ca41
nodetator <nodetator@gmail.com>
2022-11-06 20:51:58 +0800

    Finished Javascript functions! surprisingly easy

---
+++ javascript.js
@@ -0,0 +1,20 @@
+function add7(x) {
+    x += 7;
+    return x;
+}
+
+function multiply(a, b) {
+    return a * b; 
+}
+
+function capitalize (x) {
+    y = x.slice(1);
+    x =  x.charAt(0);
+    x = x.toUpperCase();
+    return x + y;
+}
+
+function lastLetter (x) {
+    x = x.slice(-1);
+    return x;
+}
